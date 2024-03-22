b462b5f1f7da388ecbc8ee5f0c1c9ffcdbb88d20
xpycteam88 <sicuno@mail.ru>
2024-03-22 20:41:11 +0300

    Init commit

---
+++ src/main/java/com/example/cw_javacore/CwJavaCoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.cw_javacore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CwJavaCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CwJavaCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cw_javacore/CwJavacoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cw_javacore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CwJavacoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
